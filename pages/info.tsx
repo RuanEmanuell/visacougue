359d304a0f5436622911d70c82b1f89ba161001d
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-05-27 01:18:23 -0300

    (Version 1.0) Starting info design

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,24 +1,39 @@
-import { Text, View, SafeAreaView, TextInput, Pressable, Dimensions, ScrollView, FlatList } from 'react-native';
+import { Text, View, SafeAreaView, TextInput, Pressable, Dimensions, ScrollView, FlatList, ActivityIndicator } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import UserData from '../utils/userdata';
-import { Appbar } from 'react-native-paper';
+import { Appbar, FAB, Icon } from 'react-native-paper';
 import { homeStyle } from '../styles/home';
 import DSGovInput from '../components/input';
 import { db } from '../utils/firebaseconfig';
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 export default function InfoScreen({ route, navigation }) {
   const user: UserData = route.params['user'];
 
-  const [blocks, setBlocks] : any = useState([]);
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
 
-  async function fetchData(){
+  const [blocks, setBlocks]: any = useState([]);
+
+  async function fetchData() {
     try {
       const queryBlocks = await getDocs(collection(db, 'blocks'));
-      const blockList = queryBlocks.docs.map(block => ({id: block.id, ...block.data()}));
+      const blockList = queryBlocks.docs.map(block => ({ id: block.id, ...block.data() }));
       setBlocks(blockList);
     } catch (error) {
-      console.error(error); 
+      console.error(error);
+    }
+  }
+
+  async function addBlock() {
+    try {
+      await addDoc(collection(db, 'blocks'), {
+        blockIndex: 2,
+        name: 'Equipamentos, Móveis e Utensílios'
+      })
+      fetchData();
+    } catch (error) {
+      console.error(error);
     }
   }
 
@@ -28,25 +43,34 @@ export default function InfoScreen({ route, navigation }) {
 
   return (
     <View style={{ backgroundColor: '#fff', flex: 1 }}>
-      <Appbar.Header style = {{backgroundColor: '#fff'}}>
-        <Appbar.Action icon='arrow-left' onPress={() => {navigation.navigate('home', {user: user})}} />
-        <Appbar.Content title = 'Informativo' titleStyle = {{textAlign: 'center', fontWeight: 'bold'}}></Appbar.Content>
+      <Appbar.Header style={{ backgroundColor: '#fff' }}>
+        <Appbar.Action icon='arrow-left' onPress={() => { navigation.navigate('home', { user: user }) }} />
+        <Appbar.Content title='Informativo' titleStyle={{ textAlign: 'center', fontWeight: 'bold' }}></Appbar.Content>
       </Appbar.Header>
       <SafeAreaView style={homeStyle.container}>
-        <DSGovInput onChangeText={() => {}} secureTextEntry = {false} placeholder='Buscar uma informação...'></DSGovInput>
+        <DSGovInput onChangeText={() => { }} secureTextEntry={false} placeholder='Buscar uma informação...'></DSGovInput>
+        {blocks ?
+        <FlatList
+        style = {{width: '100%', marginTop: 20}}
+          data={blocks}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={{ borderColor: 'black', borderWidth: 2, height: windowHeight/3.5, width: '85%', alignSelf: 'center', marginVertical: windowHeight/30 }}>
+              <Text style = {{fontWeight: 'bold', fontSize: 16, textAlign: 'center'}}>{item.name}</Text>
+              <Text style = {{color: 'grey', fontWeight: 'bold', fontSize: 16, position: 'absolute', bottom: 0, left: 5}}>Bloco {item.blockIndex}</Text>
+              <Pressable onPress={() => {}} style = {{position : 'absolute', bottom: 0, right: 5}}>
+                <Icon source = 'circle-edit-outline' size = {36}></Icon>
+              </Pressable>
+            </View>
+          )}
+        /> : <ActivityIndicator size='large' color='#1351B4' />}
+      <FAB
+        style={{ backgroundColor: '#1351B4', position: 'absolute', bottom: 2}}
+        color='white'
+        icon='plus'
+        onPress={addBlock}
+      />
       </SafeAreaView>
-      <FlatList
-      data={blocks}
-      keyExtractor={(item) => item.id}
-      renderItem={({item}) => (
-        <View style = {{borderColor: 'black', borderWidth: 2}}>
-          <Text>Bloco {item.blockIndex}</Text>
-          <Text>{item.name}</Text>
-        </View>
-      )}
-      >
-
-      </FlatList>
     </View>
   );
 }
