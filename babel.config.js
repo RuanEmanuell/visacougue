be406860149072da1f9ca509c51518a22390b4f9
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-05-29 00:28:33 -0300

    (Version 1.0) Starting camera roll

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ["expo-image-picker",
+        {
+          "photosPermission": "The app accesses your photos to let you share them with your friends."
+        },
+      ],
       ['module:react-native-dotenv']
     ]
   };
--- pages/add.tsx
+++ pages/add.tsx
@@ -1,16 +1,20 @@
-import { Text, View, SafeAreaView, TextInput, Pressable, Dimensions, ScrollView } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Dimensions, ScrollView, View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
 import UserData from '../utils/userdata';
 import { Appbar } from 'react-native-paper';
 import { homeStyle } from '../styles/home';
+import DSGovInput from '../components/input';
+import DSGovButton from '../components/button';
 import { MaterialIcons } from '@expo/vector-icons';
-import HomeOption from '../components/option';
 
 export default function AddScreen({ route, navigation }) {
   const user: UserData = route.params;
-  
+
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
+
+  const [blockName, setBlockName] = useState('');
+  const [blockImage, setBlockImage] = useState(null);
 
   return (
     <ScrollView contentContainerStyle={{ backgroundColor: '#fff', flex: 1 }}>
@@ -19,7 +23,18 @@ export default function AddScreen({ route, navigation }) {
         <Appbar.Content title='Adicionar informação' titleStyle={{ textAlign: 'center', fontWeight: 'bold' }} />
       </Appbar.Header>
       <SafeAreaView style={homeStyle.container}>
-
+        <View style={{ borderColor: 'black', borderWidth: 3, width: windowWidth / 1.2, height: windowHeight / 2.5, display: 'flex', alignItems: 'center', paddingTop: 10 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Nome do bloco:</Text>
+          <DSGovInput
+            placeholder='Digite o nome do bloco...'
+            onChangeText={(text) => { setBlockName(text) }}
+          />
+          {blockImage ? <Image /> :
+            <View style={{ marginTop: windowHeight/50, backgroundColor: 'lightgray', width: '66%', height: '50%', justifyContent: 'center', alignItems: 'center' }}>
+              <MaterialIcons name={'photo'} color='black' size={96} style={{ marginRight: 12 }}></MaterialIcons>
+            </View>}
+          <DSGovButton primary label='Adicionar imagem' onPress={() => { }} />
+        </View>
       </SafeAreaView>
     </ScrollView>
   );
