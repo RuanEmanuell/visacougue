ca346ab2a942bc859cf7672ecf51d88b74bcc6e2
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-07-25 10:53:39 -0300

    (Version 1.0) Changing colors, adding percentage bar

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView, Dimensions, ScrollView, View, Text } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import UserData from '../utils/interfaces/userdata';
 import { Appbar, Checkbox } from 'react-native-paper';
 import { simulatorInfo, simulatorInfoMap } from '../utils/info/simulator';
@@ -10,29 +10,34 @@ export default function SimulatorScreen({ route, navigation }) {
 
     const windowHeight = Dimensions.get('window').height;
 
+    const scrollRef = useRef<ScrollView>(null);
+
     const [concludedPercentage, setConcludedPercentage] = useState(0);
     const [currentSimulatorInfo, setCurrentSimulatorInfo] = useState(0);
 
     function setNewSimulatorPercentage(){
-        setConcludedPercentage(prev => prev  + (100 / simulatorInfo[simulatorInfoMap[currentSimulatorInfo]].length));
-        setCurrentSimulatorInfo(prev => prev + 1);
+        scrollRef.current?.scrollTo({ y: 0, animated: true });
+        setConcludedPercentage(prev => prev < 80 ? prev  + 16 : 100);
+        setCurrentSimulatorInfo(prev => prev < 5 ? prev + 1 : prev);
     }
 
     return (
-        <ScrollView contentContainerStyle={{ backgroundColor: '#fff', flexGrow: 1 }}>
+        <ScrollView contentContainerStyle={{ backgroundColor: '#fff', flexGrow: 1 }} ref={scrollRef}>
             <Appbar.Header style={{ backgroundColor: '#fff' }}>
                 <Appbar.Action icon='arrow-left' onPress={() => { navigation.navigate('home', { userData: user }) }} />
                 <Appbar.Content title='Simulador' titleStyle={{ textAlign: 'center', fontWeight: 'bold' }} />
             </Appbar.Header>
             <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', width: '100%' }}>
                 <Text style={{ fontWeight: 'bold' }}>{concludedPercentage}% concluído</Text>
-                <View style={{ borderColor: 'black', borderWidth: 2, width: '80%', height: 30, marginTop: 5 }}></View>
+                <View style={{ borderColor: 'black', borderWidth: 2, width: '80%', height: 30, marginTop: 5 }}>
+                    <View style = {{backgroundColor: 'green', width: `${concludedPercentage}%`, height: 26}}></View>
+                </View>
                 <Text style={{ fontWeight: 'bold', marginTop: 30, marginBottom: 10 }}>Perguntas de {simulatorInfoMap[currentSimulatorInfo]}</Text>
                 <View style={{ borderColor: 'black', borderWidth: 2, marginHorizontal: 30, paddingVertical: 10, marginVertical: 10 }}>
                     {simulatorInfo[simulatorInfoMap[currentSimulatorInfo]].map((item, key) => (
                         <View style={{ display: 'flex', width: '100%' }}>
                             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginVertical: 5, marginRight: 40 }}>
-                                <Checkbox status='checked' />
+                                <Checkbox status='checked' color='#1351B4'/>
                                 <Text key={key}>{item}</Text>
                             </View>
                         </View>
