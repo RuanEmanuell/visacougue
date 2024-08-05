323eca797b016671624d5fa9aef98d001fad8f66
RuanEmanuell <ruanvitorcom540@gmail.com>
2024-08-05 09:34:18 -0300

    (Version 1.0) Adding password on input

--- components/input.tsx
+++ components/input.tsx
@@ -1,5 +1,6 @@
-import { TextInput } from "react-native";
-import React from "react";
+import { TextInput, View, Pressable } from "react-native";
+import React, { useState } from "react";
+import FontAwesome from '@expo/vector-icons/build/FontAwesome';
 
 interface inputProps {
     onChangeText: (arg: string) => any,
@@ -12,27 +13,33 @@ interface inputProps {
 }
 
 export default function DSGovInput({ onChangeText, secureTextEntry, placeholder, value, multiline, textAlign, maxLength }: inputProps) {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(secureTextEntry ? !secureTextEntry : false);
+
     return (
-        <TextInput
-            value={value}
-            placeholder={placeholder}
-            onChangeText={text => onChangeText(text)}
-            secureTextEntry={secureTextEntry ? secureTextEntry : false}
-            multiline={multiline ? multiline : false}
-            maxLength={maxLength ? maxLength : 10000}
-            style={{
-                borderColor: 'black',
-                borderWidth: 1,
-                borderRadius: 5,
-                color: 'black',
-                fontSize: 20,
-                fontWeight: 'bold',
-                width: '75%',
-                height: multiline ? 'auto' : 48,
-                minHeight: multiline ? 72 : 48,
-                marginTop: 16,
-                paddingHorizontal: 8,
-                textAlign: textAlign ? textAlign : 'start'
-            }}></TextInput>
-    )
+        <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'black', borderWidth: 1, borderRadius: 5, width: '75%', marginTop: 16 }}>
+            <TextInput
+                value={value}
+                placeholder={placeholder}
+                onChangeText={text => onChangeText(text)}
+                secureTextEntry={secureTextEntry ? isPasswordVisible : false}
+                multiline={multiline ? multiline : false}
+                maxLength={maxLength ? maxLength : 10000}
+                style={{
+                    color: 'black',
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    height: multiline ? 'auto' : 48,
+                    minHeight: multiline ? 72 : 48,
+                    paddingHorizontal: 8,
+                    flex: 1,
+                    textAlign: textAlign ? textAlign : 'start'
+                }}
+            />
+            {secureTextEntry && (
+                <Pressable onPress={() => setIsPasswordVisible(!isPasswordVisible)} style={{ padding: 8 }}>
+                    <FontAwesome name={isPasswordVisible ? 'eye' : 'eye-slash'} size={24} color='grey' />
+                </Pressable>
+            )}
+        </View>
+    );
 }
